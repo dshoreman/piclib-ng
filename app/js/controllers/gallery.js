1abc87441e64e13428c9c9e394b45a5c29add001
Dave Shoreman <code@daveshoreman.com>
2014-06-18 01:04:31 +0100

    Add a bunch of extra data for views
    This is pretty how much how I want the data from the API. One or two
    bits might change, but I'll use this model when I do the Laravel side.

--- app/js/controllers/gallery.js
+++ app/js/controllers/gallery.js
@@ -1,7 +1,85 @@
 function galleryCtrl ($scope) {
 	$scope.albums = [
-		{name: "Baby's First Footsteps"},
-		{name: "Fun Times in Finland"},
-		{name: "Laravel in le Dam!"}
+		{
+			id: 1,
+			name: "Baby's First Footsteps",
+			description: "Pictures of our baby learning to walk. So cute!",
+			images: [
+				{
+					id: 1,
+					name: "",
+					description: "",
+					url: ""
+				}
+			]
+		},
+		{
+			id: 2,
+			name: "Fun Times in Finland",
+			description: "Bit bloody cold out there...",
+			images: [
+				{
+					id: 4,
+					name: "Hotel Kakslauttanen",
+					description: "Hotel Kakslauttanen is the home of the famous Igloo Village with the unique Glass and Snow Igloos and a World's Largest Snow Restaurant",
+					url: "https://farm3.staticflickr.com/2300/2380506976_6cf2e5355c_b.jpg"
+				},
+				{
+					id: 2,
+					name: "Made in Finland",
+					description: "Siikajärvi, Lapland",
+					url: "https://farm6.staticflickr.com/5091/5421580052_076bbcf18c_b.jpg"
+				},
+				{
+					id: 7,
+					name: "Time! It Goes Too Fast.",
+					description: "",
+					url: "https://pbs.twimg.com/profile_background_images/459004171450986496/Rl5l2JvJ.jpeg"
+				},
+				{
+					id: 7,
+					name: "Aurora Borealis",
+					description: "",
+					url: "https://farm4.staticflickr.com/3769/9662516143_55e1ebbd7d_h.jpg"
+				}
+			]
+		},
+		{
+			id: 3,
+			name: "Laravel in le Dam!",
+			description: "Much code, such smoke. Wow!",
+			images: [
+				{
+					id: 5,
+					name: "",
+					description: "",
+					url: ""
+				},
+				{
+					id: 6,
+					name: "",
+					description: "",
+					url: ""
+				},
+				{
+					id: 3,
+					name: "",
+					description: "",
+					url: ""
+				},
+				{
+					id: 8,
+					name: "",
+					description: "",
+					url: ""
+				},
+				{
+					id: 9,
+					name: "",
+					description: "",
+					url: ""
+				}
+			]
+		}
 	];
 }
